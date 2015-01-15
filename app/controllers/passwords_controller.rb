397fd209994935e64f2db0b2f9ba392a924b55a3
Mark Daniels <mark_daniels1982@hotmail.com>
2015-01-15 07:02:48 -0800

    initial commit

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -17,4 +17,26 @@ class PasswordsController < ApplicationController
     redirect_to login_url, notice: "Email was sent with instructions"
 
   end
+
+  def edit
+    code = params[:id]
+    @user = User.find_by(code: code)
+    puts "THIS IS USER \n \n \n \n \n"
+    puts user
+  end
+
+  def update
+    code = params[:id]
+    @user = User.find_by(code: code)
+    if @user.update_attributes param.require(:user).permit(:password, :password_confirmation)
+      #invalidate the @user.code to avoid replay
+      @user.code = nil
+      @user.save
+      redirect_to root_path
+    else
+      redirect_to "/passwords/#{@user.code}"
+    end
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "site#index"
   get "/login" => "session#new"
   post "/login" => "session#create"
+  post "/passwords/:id", to "passwords#update"
 
   delete "/logout" => "session#destroy"
   get "/logout" => "session#destroy" #TODO: DELETE THIS BEFORE PRODUCTION
